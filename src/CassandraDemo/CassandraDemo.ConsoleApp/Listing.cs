790f4861ed331513833383a63040cad63a34d91b
matt blanchard <matthewablanchard@gmail.com>
2017-07-16 02:30:53 -0500

    Added csharp cassandra driver demo

---
+++ src/CassandraDemo/CassandraDemo.ConsoleApp/Listing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CassandraDemo.ConsoleApp
+{
+    [DebuggerDisplay("{Name} - {City}")]
+    public class Listing
+    {
+        public string City { get; set; }
+        public int ListingId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Description { get; set; }
+        public double AverageReview { get; set; }
+    }
+}
---
+++ src/CassandraDemo/CassandraDemo.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Cassandra;
+using Cassandra.Data.Linq;
+using Cassandra.Mapping;
+using System;
+
+namespace CassandraDemo.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter node IP: ");
+            var ip = Console.ReadLine();
+            Console.WriteLine($"Enter password: ");
+            var password = Console.ReadLine();
+            using (var cluster = Cluster.Builder().WithCredentials("cassandra", password).AddContactPoints(ip).Build())
+            using (var session = cluster.Connect("demo")) {
+                //Raw CQL
+                var rowSets = session.Execute("SELECT * FROM listings");
+
+                MappingConfiguration.Global.Define(
+                    new Map<Listing>()
+                        .TableName("listings")
+                        .PartitionKey(u => u.ListingId)
+                        .Column(u => u.ListingId, cm => cm.WithName("listing_id"))
+                        .Column(u => u.AverageReview, cm => cm.WithName("average_review"))
+                );
+
+                //records<=>entities with linq support
+                var listingsTable = new Table<Listing>(session);
+                while (true)
+                {
+                    Console.WriteLine($"Enter an ID: ");
+                    var keyStroke = Console.ReadKey();
+                    int id;
+                    if (keyStroke.KeyChar == 'q') { break; }
+                    
+                    else if (int.TryParse(keyStroke.KeyChar.ToString(), out id))
+                    {
+                        var listing = listingsTable.FirstOrDefault(x => x.ListingId == id).Execute();
+                        Console.WriteLine($"\n\n\tName: {listing?.Name ?? "N/A"}\n\tCity: {listing?.City ?? "N/A"}\n\tState: {listing?.State ?? "N/A"}");
+                    }
+                    
+                }
+                
+            }
+
+        }
+    }
+}
